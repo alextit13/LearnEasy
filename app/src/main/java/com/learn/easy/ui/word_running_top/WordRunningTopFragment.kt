f373db9d9f6094a4ea648ef44aacfc3b10e3153b
Alexey <alexcas@mail.ru>
2020-01-02 20:52:29 +0300

    jv

--- app/src/main/java/com/learn/easy/ui/word_running_top/WordRunningTopFragment.kt
+++ app/src/main/java/com/learn/easy/ui/word_running_top/WordRunningTopFragment.kt
@@ -1,31 +1,44 @@
 package com.learn.easy.ui.word_running_top
 
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.learn.easy.R
+import com.learn.easy.ui.BaseFragment
+import kotlinx.android.synthetic.main.fragment_word_running_top.*
+import kotlinx.coroutines.*
 
-class WordRunningTopFragment : Fragment() {
+class WordRunningTopFragment : BaseFragment(R.layout.fragment_word_running_top) {
 
-    private lateinit var shareViewModel: WordRunningTopViewModel
+    private val viewModel: WordRunningTopViewModel by lazy {
+        ViewModelProviders.of(this).get(WordRunningTopViewModel::class.java)
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        shareViewModel =
-            ViewModelProviders.of(this).get(WordRunningTopViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_word_running_top, container, false)
-        val textView: TextView = root.findViewById(R.id.text_share)
-        shareViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        runScroller()
+    }
+
+    private fun initListeners() {
+        fabOpenStringRunningTop.setOnClickListener {  }
+        tvTextStringRunningTop.apply {
+            text = "You are almost there. Just add a few more things to achieve this, First of all add those properties in your TextView You are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextViewYou are almost there. Just add a few more things to achieve this, First of all add those properties in your TextView"
+        }
+        svWordRunningTop.fullScroll(View.FOCUS_DOWN)
+    }
+
+    private fun runScroller() {
+        var scrollPosition = 0
+        GlobalScope.launch {
+            while (true) {
+                withContext(Dispatchers.Main) {
+                    svWordRunningTop.smoothScrollTo(0, scrollPosition)
+                }
+                delay(10)
+                scrollPosition++
+            }
+        }
     }
 }
--- app/src/main/java/com/learn/easy/ui/word_running_top/WordRunningTopViewModel.kt
+++ app/src/main/java/com/learn/easy/ui/word_running_top/WordRunningTopViewModel.kt
@@ -1,13 +1,7 @@
 package com.learn.easy.ui.word_running_top
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WordRunningTopViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is running top words"
-    }
-    val text: LiveData<String> = _text
+    // todo this
 }
